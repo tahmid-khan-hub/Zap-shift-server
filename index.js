4d555ac0282401f8be3ce444731f7f9aeb466206
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-06-24 20:16:55 +0600

    added get method for parcels

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zc7c13h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,15 +18,14 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-   
     await client.connect();
 
-    const parcelCollection = client.db('parcelDB').collection('parcels');
+    const parcelCollection = client.db("parcelDB").collection("parcels");
 
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
@@ -45,19 +43,35 @@ async function run() {
       res.send(parcels);
     });
 
+    app.get("/parcels", async (req, res) => {
+      const userEmail = req.query.email;
 
+      const query = userEmail ? { email: userEmail } : {};
 
+      try {
+        const parcels = await parcelCollection
+          .find(query)
+          .sort({ creation_date: -1 }) // Newest first
+          .toArray();
+
+        res.send(parcels);
+      } catch (error) {
+        console.error("Failed to fetch parcels:", error);
+        res.status(500).send({ error: "Failed to fetch parcels." });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 // Basic Route
 app.get("/", (req, res) => {
